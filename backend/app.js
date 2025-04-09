bd6925c86c50bc4413f1c4d009d1ccca41003606
Edhem Rogo <edhem.rogo@stu.ibu.edu.ba>
2025-04-09 14:12:23 +0200

    Google authentication initialised

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,49 @@
 const express = require('express');
-const cors = require('cors');
+const session = require('express-session');
+const passport = require('passport');
 require('dotenv').config();
+require('./config/auth');
 
-const PORT = process.env.PORT;
+function isLoggedIn(req, res, next) {
+    req.user ? next(): res.sendStatus(401);
+}
+
 const app = express();
 app.use(express.json());
 
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+
+app.get('/', (req, res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>');
+})
+
+app.get('/auth/google', 
+    passport.authenticate('google', {scope: ['email', 'profile']})
+);
+
+app.get('/google/callback', 
+    passport.authenticate('google', {
+        successRedirect: '/protected',
+        failureRedirect: '/'
+    })
+);
+
+
+
+app.get('/protected', isLoggedIn, (req, res) => {
+    const googleId = req.user.id;
+    
+    // Send back user information 
+    res.json({
+        message: "You're authenticated!",
+        user: req.user,
+        googleId: googleId
+    });
 });
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
+})
